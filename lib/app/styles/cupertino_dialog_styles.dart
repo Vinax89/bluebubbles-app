a57587d76315ddc4acc6d5000a532791113111e6
Vinax89 <migz89star@gmail.com>
2025-08-25 23:47:43 -0700

    refactor: extract cupertino dialog text styles

---
+++ lib/app/styles/cupertino_dialog_styles.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+const TextStyle kCupertinoDialogTitleStyle = TextStyle(
+  fontFamily: '.SF UI Display',
+  inherit: false,
+  fontSize: 17.0,
+  fontWeight: FontWeight.w600,
+  height: 1.3,
+  letterSpacing: -0.5,
+  textBaseline: TextBaseline.alphabetic,
+);
+
+const TextStyle kCupertinoDialogContentStyle = TextStyle(
+  fontFamily: '.SF UI Text',
+  inherit: false,
+  fontSize: 13.0,
+  fontWeight: FontWeight.w400,
+  height: 1.35,
+  letterSpacing: -0.2,
+  textBaseline: TextBaseline.alphabetic,
+);
+
+const TextStyle kCupertinoDialogActionStyle = TextStyle(
+  fontFamily: '.SF UI Text',
+  inherit: false,
+  fontSize: 16.8,
+  fontWeight: FontWeight.w400,
+  textBaseline: TextBaseline.alphabetic,
+);
